e6b4b8ada546b7beea1a3651fa83aaa73e251568
Sselentt <sselentt23@gmail.com>
2023-11-22 01:52:06 +0300

    Console - Northwınd Code First

--- CA_Northwınd/Entities/Supplier.cs
+++ CA_Northwınd/Entities/Supplier.cs
@@ -8,7 +8,7 @@
         public string? ContactName { get; set; }
         public string? ContactTitle { get; set; }
         public string? Address { get; set; }
-
+        
 
         //bir tedarikçini bir çok ürünü olur
         public virtual List<Product> Products { get; set; }
